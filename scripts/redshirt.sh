125487fbf9099348c33109cf9e2b95b857eff18e
FSGSebastian <sebastian@frozenlight.de>
2014-08-03 19:43:13 +0200

    Red Shirt Emulator

---
+++ scripts/redshirt.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+while :
+do
+	echo "Hey, press a key"
+	sleep 1
+	echo ">> Which key?"
+	sleep 1
+	echo "Any key"
+	sleep 1
+	echo ">> There is no any key"
+	sleep 1
+	echo "Ok, I do it"
+	sleep 1
+	xdotool key "F16"
+	echo 
+done
